ab706bc40f34f30e6ee94f2c8a785c770ba71186
Howie Chien <howie.chien@moblab.com>
2025-06-16 13:33:04 +0800

    Update .gitignore to include Jupyter notebook files and modify db.py to use environment variables for MongoDB credentials.

--- config/db.py
+++ config/db.py
@@ -15,8 +15,8 @@ MONGOCLIENT_LOCAL = MongoClient(
 
 MONGOCLIENT = MongoClient(
     "mongodb+srv://cluster0.ni3d51g.mongodb.net",
-    username="howie_admin",
-    password="4SUiGfooCdIDSlPZ",
+    username=os.environ.get("MONGO_USERNAME"),
+    password=os.environ.get("MONGO_PASSWORD"),
     serverSelectionTimeoutMS=30000,  # 30 seconds
     socketTimeoutMS=30000,  # 30 seconds
     connectTimeoutMS=30000,  # 30 seconds

a29d5b22d52d9ea415931a8ded9c9e34b76aa5f1
Howie Chien <howie.chien@moblab.com>
2025-06-16 13:30:11 +0800

    Update database connection logic to use MONGOCLIENT based on environment; uncomment MONGOCLIENT configuration in db.py; adjust imports in test notebook.

--- app/_utils/db_setup.py
+++ app/_utils/db_setup.py
@@ -1,11 +1,14 @@
 from import_setup import *
-from config.db import MONGOCLIENT_LOCAL
+from config.db import MONGOCLIENT_LOCAL, MONGOCLIENT
 import streamlit as st
 
 
 @st.cache_resource
 def connect_to_db(db="cherry_picking_trap"):
-    return MONGOCLIENT_LOCAL[db]
+    if os.environ.get("ENV") == "streamlit":
+        return MONGOCLIENT[db]
+    else:
+        return MONGOCLIENT_LOCAL[db]
 
 
 db = connect_to_db()
--- config/db.py
+++ config/db.py
@@ -13,14 +13,14 @@ MONGOCLIENT_LOCAL = MongoClient(
 )
 
 
-# MONGOCLIENT = MongoClient(
-#     "mongodb+srv://cluster0.ni3d51g.mongodb.net",
-#     username="howie_admin",
-#     password="4SUiGfooCdIDSlPZ",
-#     serverSelectionTimeoutMS=30000,  # 30 seconds
-#     socketTimeoutMS=30000,  # 30 seconds
-#     connectTimeoutMS=30000,  # 30 seconds
-#     maxPoolSize=50,
-#     retryWrites=True,
-#     retryReads=True,
-# )
+MONGOCLIENT = MongoClient(
+    "mongodb+srv://cluster0.ni3d51g.mongodb.net",
+    username="howie_admin",
+    password="4SUiGfooCdIDSlPZ",
+    serverSelectionTimeoutMS=30000,  # 30 seconds
+    socketTimeoutMS=30000,  # 30 seconds
+    connectTimeoutMS=30000,  # 30 seconds
+    maxPoolSize=50,
+    retryWrites=True,
+    retryReads=True,
+)
